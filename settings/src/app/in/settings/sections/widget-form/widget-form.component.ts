f82edb5734b6ea048bd723a455de9de0ce312e4c
Crosimo <jean.deborsu@hotmail.com>
2022-01-30 20:11:17 +0100

    widget form submit

--- settings/src/app/in/settings/sections/widget-form/widget-form.component.ts
+++ settings/src/app/in/settings/sections/widget-form/widget-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { pairwise, startWith } from 'rxjs/operators';
 import { SettingService } from 'src/app/settingService';
 
 @Component({
@@ -9,16 +11,28 @@ import { SettingService } from 'src/app/settingService';
 export class WidgetFormComponent implements OnInit {
   @Input() setting : any;
   public settingValue: any;
+  public control=new FormControl();
   constructor(public service : SettingService) { }
 
   ngOnInit(): void {
+
+
+    this.control.valueChanges.pipe(
+      startWith(this.settingValue),
+      pairwise()
+    ).subscribe(
+      ([old,value])=>{
+
+        this.settingValue = value;
+      }
+    )
   }
 
   public change(event:any){
-    console.log(event);
-    this.service.fillQueue(this.setting.id, {newValue: "", oldValue: this.settingValue}).subscribe((r)=>{
+    
+    this.service.fillQueue(this.setting.id, {newValue: this.settingValue, oldValue: ""}).subscribe((r)=>{
       if(this.settingValue == r){
-        // eventValue.source.value = this.settingValue;
+        event.source.value = this.settingValue;
       }
       this.settingValue=r});
   }
