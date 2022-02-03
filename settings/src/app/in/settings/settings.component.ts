12878c782fb85f396e60fc316cfc1efdf282a520
unknown <cedricfrancoys@gmail.com>
2022-02-03 10:25:03 +0100

    updated settings

--- settings/src/app/in/settings/settings.component.ts
+++ settings/src/app/in/settings/settings.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'sb-shared-lib';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router'
-import { environment } from 'src/environments/environment';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 
 @Component({
@@ -10,99 +9,70 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+
   constructor(
     private api: ApiService,
     private route: ActivatedRoute,
     private router: Router,
   ) { }
 
-  // all the data
-  public data: any[] = [];
-  //data sorted by sections
-  public dataSorted: any[] = [];
-  //sorted section names
-  public nameArray: string[] = [];
-
   public settingRoute:string
+  private package: string = 'core';
 
+  // data sorted by sections
+  public sections: Array<any> = new Array();
+
+  public sectionsMap: any = {};
+
+  public sectionsDescriptions: any = {
+    'locale': 'Paramètres régioniaux',
+    'main': 'Paramètres généraux et de formats',
+    'security': 'Paramètres de sécurité',
+    'units': 'Unités de mesures et systèmes d\'unités'
+  };
 
   ngOnInit() {
-
     // Gets the right DATA for the right ROUTE PARAM
     this.route.params.subscribe(async params => {
-      try {
-        if (params.package == "general") {
-          this.data = await this.api.collect('core\\Setting', ['package', '=', 'core'], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'title', 'help', 'form', 'setting_choices_ids.value'], environment.lang);
-          console.log(this.data);
-          this.sortData();
-          this.settingRoute =  'SETTINGS_LIST_GENERAL';
-        } else if (params.package == "sale") {
-          this.data = await this.api.collect('core\\Setting', ['package', '=', 'sale'], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], environment.lang);
-          console.log(this.data);
-          this.sortData();
-          this.settingRoute =  'SETTINGS_LIST_SALE';
-        } else {
-          this.data = await this.api.collect('core\\Setting', ['package', '=', 'finance'], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], environment.lang);
-          this.sortData();
-          this.settingRoute =  'SETTINGS_LIST_FINANCE';
-        }
-      }
-      catch (err) {
-        console.log('erreur');
-      }
-      console.log(this.data);
+      this.package = params.package;
+      this.settingRoute =  'SETTINGS_LIST_' + this.package.toUpperCase();
+      this.reset();
     });
 
-
-    // Allows us to switch the data (with initialize) when the route parameter changes
-    this.router.events.subscribe((val) => {
+    // Allow to switch the data (with initialize) when the route parameter changes
+    this.router.events.subscribe( async (val) => {
       if (val instanceof NavigationEnd) {
-        //resets the data array's
-        this.initialize();
+        this.reset().then( () => '' );
       }
     });
   }
 
+  /**
+   * Initialise the payload based on current route
+   * 
+   */  
+  public async reset() {
+    try {
+      const data: any[] = await this.api.collect(
+        'core\\Setting', 
+        ['package', '=', this.package], 
+        ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], 
+        'id', 'asc', 0, 100
+      );
 
-
-
-  // Sorting the arrays by Section ex: "locale, security, ..." 
-  public sortData() {
-    // First we reset the array's
-    this.dataSorted = [];
-    this.nameArray = [];
-    this.data.forEach(element => {
-      if (!this.nameArray.includes(element.section)) {
-        this.nameArray.push(element.section);
-        let index = this.nameArray.indexOf(element.section);
-        this.dataSorted[index] = [element];
-      } else {
-        let index = this.nameArray.indexOf(element.section);
-        this.dataSorted[index].push(element);
-      }
-    });
-  }
-
-
-
-  // We check the new route
-  public initialize() {
-
-    this.route.params.subscribe(async params => {
-      if (params.package == "general") {
-        this.data = await this.api.collect('core\\Setting', ['package', '=', ' '], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], environment.lang);
-        this.sortData();
-        this.settingRoute =  'SETTINGS_LIST_GENERAL';
-
-      } else if (params.package == "sale") {
-        this.data = await this.api.collect('core\\Setting', ['package', '=', 'sale'], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], environment.lang);
-        this.sortData();
-        this.settingRoute =  'SETTINGS_LIST_SALE';
-      } else {
-        this.data = await this.api.collect('core\\Setting', ['package', '=', 'finance'], ['package', 'section', 'description', 'setting_values_ids.value', 'type', 'setting_choices_ids.value', 'title', 'help', 'form'], environment.lang);
-        this.sortData();
-        this.settingRoute =  'SETTINGS_LIST_FINANCE';
-      }
-    });
+      // reset the array
+      this.sections = [];
+      this.sectionsMap = {};
+      data.forEach(element => {
+        if(!this.sectionsMap.hasOwnProperty(element.section)) {
+          this.sectionsMap[element.section] = [];
+          this.sections.push(element.section);
+        }
+        this.sectionsMap[element.section].push(element);
+      });
+    }
+    catch(error) {
+      console.log('something went wrong');
+    }
   }
 }
