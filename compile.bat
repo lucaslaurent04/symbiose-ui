1df67b927d612a0d6913f34f9fcd5de4647e0a9f
unknown <cedricfrancoys@gmail.com>
2022-02-25 17:10:18 +0100

    added files

---
+++ compile.bat
@@ -0,0 +1,7 @@
+@echo off
+cd apps & call compile.bat & cd .. 
+cd auth & call compile.bat & cd .. 
+cd booking & call compile.bat & cd .. 
+cd documents & call compile.bat & cd .. 
+cd inventory & call compile.bat & cd .. 
+cd settings & call compile.bat & cd ..
