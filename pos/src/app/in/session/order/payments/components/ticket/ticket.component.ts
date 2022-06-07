f7799e646b7c587ed008e8a8826d3cc241200aa9
Crosimo <jean.deborsu@hotmail.com>
2022-06-07 11:29:29 +0200

    added welcome screens

--- pos/src/app/in/session/order/payments/components/ticket/ticket.component.ts
+++ pos/src/app/in/session/order/payments/components/ticket/ticket.component.ts
@@ -18,13 +18,10 @@ export class TicketComponent implements OnInit {
   async ngOnInit() {
   }
   async ngOnChanges(changes: SimpleChanges) {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-    console.log("deleted !!")
     this.orders = await this.api.collect("sale\\pos\\OrderLine", ['order_id', '=', this.item?.id], ['order_id', 'price', 'name']);
     this.payments = await this.api.collect("sale\\pos\\OrderPayment", ['order_id', '=', this.item?.id], ['order_payment_parts_ids', 'total_due', 'total_paid']);
     if(this.payments.length > 0)this.payments.forEach((element:any)=>this.totalPaid += element.total_paid);
     this.paymentsParts = await this.api.collect("sale\\pos\\OrderPaymentPart", ['order_id', '=', this.item?.id], ['order_payment_parts_ids', 'payment_method', 'amount', 'order_id']);
-    console.log(this.paymentsParts)
+    
   }
 }
